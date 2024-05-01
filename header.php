37c6bb814b811d3afdacfef0d12d4f4d964e6c33
Dmitriy Korotkov <nupellot@gmail.com>
2024-05-01 23:45:03 +0300

    Слепил header

---
+++ header.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="css/header.css">
+
+<header>
+   <div class="header__center-block">
+       <div class="header__left-side">
+           <img src="images/deocons.png" alt="" class="header__logo">
+       </div>
+       <div class="header__right_side">
+           <a href="tel:+790001234567" class="header__phone">+7 (900) 012 34-56</a>
+       </div>
+   </div>
+</header>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="description" content="Мы - производители инженерного оборудования. У нас можно купить муфты для строительной арматуры">
+    <title>deocons</title>
+    <link href="css/index.css" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+</head>
+<body>
+
+
+
+
+<?php include('header.php'); ?>
+
+<?php //include('navigation.php'); ?>
+
+
+<main>
+
+
+
+</main>
+
+
+<?php //include('footer.php'); ?>
+
+
+
+
+</body>
+</html>
