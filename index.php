cc20b9623e77ec9f7fd6d9cdeed46be585fae446
Dmitriy Korotkov <nupellot@gmail.com>
2024-05-04 03:37:16 +0300

    Начал делать главную страницу

--- index.php
+++ index.php
@@ -20,7 +20,34 @@
 
 <main>
 
+<div class="first-line">
+    <div class="first-line__text">
+        <div class="first-line__title">
+            Муфты для строительной арматуры
+        </div>
+        <div class="first-line__not-title">
+            <p class="we_suggest">Мы предлагаем:</p>
+            <p> - Изготовление всех типов муфт с конической и параллельной резьбой</p>
+            <p> - Изготовление опрессованных соединений</p>
+            <p> - Опрессовку муфт мобильными гидравлическими прессами</p>
+        </div>
+    </div>
+    <img src="images/трубы.png" alt="" class="first-line__img">
+</div>
 
+
+<div class="second-line-wrapper">
+    <div class="second-line">
+        <img src="images/future.webp" alt="" class="second-line__img">
+        <div class="second-line__text">
+            <p class="we_suggest">Наши преимущества</p>
+            <p class="second-line__advantage">Собственная производственная база в Подмосковье</p>
+            <p class="second-line__advantage">Быстрая поставка продукции на склад/объект заказчика</p>
+            <p class="second-line__advantage">Сертифицированная продукция, соответствующая требованиям ГОСТ и ТУ</p>
+            <p class="second-line__advantage">Постоянное развитие производства и ассортимента изделий</p>
+        </div>
+    </div>
+</div>
 
 </main>
 
