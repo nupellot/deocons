0df11b43d9e815a3fefcef3d5c91d085045b9f21
Dmitriy Korotkov <nupellot@gmail.com>
2024-05-14 03:26:11 +0300

    Поменял в хедере телефон на почту

--- header.php
+++ header.php
@@ -6,7 +6,8 @@
             <img src="images/deocons.png" alt="" class="header__logo">
         </div>
         <div class="header__right_side">
-            <a href="tel:+790001234567" class="header__phone">+7 (900) 012 34-56</a>
+<!--            <a href="tel:+790001234567" class="header__phone">+7 (900) 012 34-56</a>-->
+            <a href="mailto:info@deocons.su" class="header__contact">info@deocons.su</a>
         </div>
     </div>
 </header>
