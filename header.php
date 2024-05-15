dcee79380d526b100e3df27be7fbeb18ef58d803
Dmitriy Korotkov <nupellot@gmail.com>
2024-05-15 03:31:22 +0300

    Теперь лого это кнопка

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 
 <header>
     <div class="header__center-block">
-        <div class="header__left-side">
+        <a class="header__left-side" href="/">
             <img src="images/deocons.png" alt="" class="header__logo">
-        </div>
+        </a>
         <div class="header__right_side">
 <!--            <a href="tel:+790001234567" class="header__phone">+7 (900) 012 34-56</a>-->
             <a href="mailto:info@deocons.su" class="header__contact">info@deocons.su</a>
