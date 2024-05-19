ca7e5cf16925f4381572f3633e17bed28df90181
Dmitriy Korotkov <nupellot@gmail.com>
2024-05-20 00:30:17 +0300

    Изменил дизайн полосы "Наши преимущества"

--- index.php
+++ index.php
@@ -51,10 +51,22 @@
         <div class="second-line__text">
             <p class="our_advantages">Наши преимущества:</p>
             <div class="second-line__advantages">
-                <p class="second-line__advantage">Собственная производственная база в Подмосковье</p>
-                <p class="second-line__advantage">Быстрая поставка продукции на склад/объект заказчика</p>
-                <p class="second-line__advantage">Сертифицированная продукция, соответствующая требованиям ГОСТ и ТУ</p>
-                <p class="second-line__advantage">Постоянное развитие производства и ассортимента изделий</p>
+                <div class="second-line__advantage">
+                    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21h18" /><path d="M19 21v-4" /><path d="M19 17a2 2 0 0 0 2 -2v-2a2 2 0 1 0 -4 0v2a2 2 0 0 0 2 2z" /><path d="M14 21v-14a3 3 0 0 0 -3 -3h-4a3 3 0 0 0 -3 3v14" /><path d="M9 17v4" /><path d="M8 13h2" /><path d="M8 9h2" /></svg>
+                    Собственная производственная база в Подмосковье
+                </div>
+                <div class="second-line__advantage">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M17 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M5 17h-2v-11a1 1 0 0 1 1 -1h9v12m-4 0h6m4 0h2v-6h-8m0 -5h5l3 5" /></svg>
+                    Быстрая поставка продукции на склад/объект заказчика
+                </div>
+                <div class="second-line__advantage">
+                    <svg  xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 20h-11a3 3 0 0 1 0 -6h11a3 3 0 0 0 0 6h1a3 3 0 0 0 3 -3v-11a2 2 0 0 0 -2 -2h-10a2 2 0 0 0 -2 2v8" /></svg>
+                    Сертифицированная продукция, соответствующая требованиям ГОСТ и ТУ
+                </div>
+                <div class="second-line__advantage">
+                    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-iteration"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8.5 16a5.5 5.5 0 1 0 -5.5 -5.5v.5" /><path d="M3 16h18" /><path d="M18 13l3 3l-3 3" /></svg>
+                    Постоянное развитие производства и ассортимента изделий
+                </div>
             </div>
         </div>
     </div>
