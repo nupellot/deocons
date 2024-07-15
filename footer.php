e7460ee0514719d136641af03ff2e64ca1cecf7e
Dmitriy Korotkov <nupellot@gmail.com>
2024-07-16 00:15:00 +0300

    Оформил футер

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
 <link rel="stylesheet" href="css/footer.css">
 
 <footer>
-    <div>Здесь будет footer</div>
+    <div class="footer__content">
+        <div class="footer__text">
+            <img src="images/deocons-no-bg.png" alt="Логотип deocons" class="footer__logo">
+            <a class="footer__address" href="https://yandex.ru/maps/-/CDSBv454" target="_blank">
+                Московская обл., г. Чехов,
+                Симферопольское ш., 2
+            </a>
+            <div class="footer__phones">
+                Телефоны:
+                <a href="tel:+79811725663" class="footer_phone">+7 981 172-56-63</a>
+                <a href="tel:+7 921 447-42-28" class="footer_phone">+7 921 447-42-28</a>
+                <a href="tel:+7 915 169-32-22" class="footer_phone">+7 915 169-32-22</a>
+            </div>
+        </div>
+        <div class="footer__map">
+            <div style="position:relative;overflow:hidden;"><a
+                        href="https://yandex.ru/maps/10761/chehov/?utm_medium=mapframe&utm_source=maps"
+                        style="color:#eee;font-size:12px;position:absolute;top:0px;">Чехов</a><a
+                        href="https://yandex.ru/maps/10761/chehov/?ll=37.463845%2C55.161425&utm_medium=mapframe&utm_source=maps&z=17.15"
+                        style="color:#eee;font-size:12px;position:absolute;top:14px;">Яндекс Карты</a>
+                <iframe src="https://yandex.ru/map-widget/v1/?ll=37.463845%2C55.161425&z=17.15" width="560" height="400"
+                        frameborder="1" allowfullscreen="true" style="position:relative;"></iframe>
+            </div>
+        </div>
+    </div>
 </footer>
